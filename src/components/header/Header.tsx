a2afbcaf7e814232422f36949e97151cc5786e1a
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2025-05-16 20:58:14 +0300

    skeleton added to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,13 +10,14 @@ import { HeaderNavDekstop } from './HeaderNavDekstop';
 import { HeaderNavBurger } from './HeaderNavBurger';
 import NavButton from '../UI/NavButton';
 import { useAppSelector } from '@/store/store';
+import Skeleton from 'react-loading-skeleton';
 
 export const Header = () => {
   const { user } = useUser();
   const subs = useAppSelector((state) => state.subscription.subscriptions);
   const subPlan = subs[0]?.type;
-	console.log('subPlan', subPlan);
-	
+  console.log('subPlan', subPlan);
+
   return (
     <header className="flex items-center container justify-between   px-[20px]    text-white  font-medium leading-[100%]">
       <Image
@@ -44,12 +45,17 @@ export const Header = () => {
           )}
         </SignedOut>
         <SignedIn>
-				  <NavButton name={'cart'} image={'cart'} />
-				  {subPlan && (
-          <div className="flex mb-8 flex-col items-center gap-1.5">
-            <p className="text-center border-2 border-amber-300 p-1 bg-amber-200/70">{subPlan}</p>
-            <UserButton />
-          </div> )
+          <NavButton name={'cart'} image={'cart'} />
+          {subPlan ? (
+            <div className="flex mb-8 flex-col items-center gap-1.5">
+              <p className="text-center border-2 border-amber-300 p-1 bg-amber-200/70">
+                {subPlan}
+              </p>
+              <UserButton />
+            </div>
+          ) : (
+            <Skeleton width={60} height={50} />
+          )}
         </SignedIn>
       </div>
     </header>
